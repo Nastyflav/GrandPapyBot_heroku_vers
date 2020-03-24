cbdbecc2fe776480296283b00a094c3781f0b145
Flavien <flavien.murail@gmail.com>
2020-03-24 16:46:24 +0100

    All the Bootstrap elements are there

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 #! /usr/bin/env python3
 # coding: utf-8
+
+#Application informations
+APP_NAME = 'GrandPy Bot'
+APP_CATCHLINE = 'On lui a installé l\'ADSL, profitez-en ! Trouvez des lieux et leur histoire !'
+APP_AUTHOR = 'Flavien Murail'
+APP_REPO_LINK = 'https://github.com/Nastyflav/GrandPapyBot_OC'
+APP_GPL_LINK = 'https://fr.wikipedia.org/wiki/Licence_publique_g%C3%A9n%C3%A9rale_limit%C3%A9e_GNU'
+
+#Text area presets
+SIZE_AREA = 30
+TEXT_AREA = 'Coucou GrandPy, tu vas bien ? Dis-moi, tu ne saurais pas où se situe Minneapolis par hasard ?'
