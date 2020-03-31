e27a00ebea651ddcf2b0847ff2ea43ea89050dee
Flavien <flavien.murail@gmail.com>
2020-03-31 13:02:57 +0200

    Worked on the tests but Python path is fucking erratic

--- gpb_app/models/api_requests.py
+++ gpb_app/models/api_requests.py
@@ -1,23 +1,23 @@
 #! /usr/bin/env python3
 # coding: UTF-8
 
+import config as cf
 import requests as rq
 import json
 
 
 class GoogleMapsRequest:
     """Class to load Google Maps datas and map from a user query"""
-    def __init__(self):
-        self.data_list = []
+    def __init__(self, query):
+        self.query = str(query)
 
-    def location_datas(self, query):
+    def location_datas(self):
         """Make a request to the API, and sort datas"""
-        payload = {'input': query, 'inputtype': 'textquery','fields': 'formatted_address,name,geometry',\
+        payload = {'input': self.query, 'inputtype': 'textquery','fields': 'formatted_address,name,geometry',\
                         'key': 'AIzaSyCH_uGge9XRsTK22BY6zDrR2OgpqOZK204', 'language': 'fr'}
-        request = rq.get(url='https://maps.googleapis.com/maps/api/place/findplacefromtext/json?', params=payload)
-        data = request.json()
-        self.data_list.append(data)
-        print(self.data_list)
+        request = rq.get(url=cf.GOOGLE_URL, params=payload)
+        datas = request.json()
+        print(datas)
 
 
 class WikiRequest:
@@ -28,8 +28,8 @@ class WikiRequest:
 
 def main():
     
-    gm = GoogleMapsRequest()
-    gm.location_datas('Nantes')
+    gm = GoogleMapsRequest('Nantes')
+    gm.location_datas()
 
 if __name__ == "__main__":
     main()
--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -3,18 +3,29 @@
 
 import gpb_app.models.api_requests as script
 import pytest 
-
+import json
+import os
+import requests
 
 class TestGoogleMapRequest:
 
+    user_input = 'Nantes'
+    place = script.GoogleMapsRequest(user_input)
     results = {'candidates': [{'formatted_address': 'Nantes, France', \
                             'geometry': {'location': {'lat': 47.218371, 'lng': -1.553621}, \
                             'viewport': {'northeast': {'lat': 47.29582689999999, 'lng': -1.4783261}, 'southwest': {'lat': 47.1806171, 'lng': -1.6417861}}}, \
                             'name': 'Nantes'}], 'status': 'OK'}
+            
+    def test_location_datas(self, monkeypatch):
 
-    def setup_method(self):
-        self.gm_api = script.GoogleMapsRequest()
+        def mock_json_oc(*args, **kwargs):
+            with open("tests/mock_requests/nantes.json", "r") as json_file:
+                return json.loads(json_file.read())
 
+        self.place.query = self.user_input
+        monkeypatch.setattr('gpb_app.models.api_requests.GoogleMapsRequest.location_datas', mock_json_oc)
+        self.place.location_datas()
+        assert self.place.location_datas == self.results
 #   - Recevoir des données non valides ou manquantes
 #   - Récupérer un nom
 #   - Récupérer une adresse
@@ -23,10 +34,6 @@ class TestGoogleMapRequest:
 #   - Récupérer une carte
 
 
-class TestWikiRequest:
-
-    def setup_method(self):
-        self.wiki_api = script.WikiRequest()
 # API Wiki Media
 #   - Vérifier qu'il existe une page Wikipédia
 #   - Récupérer l'extrait d'une histoire
